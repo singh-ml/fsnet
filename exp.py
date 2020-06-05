434f0866e782a339bce2f789295c7965b21f7c60
dineshsinghindian <cs14resch11003@iith.ac.in>
2020-06-05 19:07:26 +0900

    Update exp.py

--- exp.py
+++ exp.py
@@ -71,50 +71,6 @@ def MI(S):
   mis=mis/count
   return mis
 
-#def clossf():
-#  return lossv
-
-class ConcreteSelect(Layer):
-  def __init__(self, output_dim, start_temp = 10.0, min_temp = 0.1, alpha = 0.99999, **kwargs):
-    self.output_dim = output_dim
-    self.start_temp = start_temp
-    self.min_temp = K.constant(min_temp)
-    self.alpha = K.constant(alpha)
-    super(ConcreteSelect, self).__init__(**kwargs)
-
-  def build(self, input_shape):
-    self.temp = self.add_weight(name = 'temp', shape = [], initializer = Constant(self.start_temp), trainable = False)
-    self.logits = self.add_weight(name = 'logits', shape = [self.output_dim, input_shape[1]], initializer = glorot_normal(), trainable = True)
-    super(ConcreteSelect, self).build(input_shape)
-    
-  def call(self, X, training = None):
-    uniform = K.random_uniform(self.logits.shape, K.epsilon(), 1.0)
-    gumbel = -K.log(-K.log(uniform))
-    temp = K.update(self.temp, K.maximum(self.min_temp, self.temp * self.alpha))
-    noisy_logits = (self.logits + gumbel) / temp
-    samples = K.softmax(noisy_logits)
-    discrete_logits = K.one_hot(K.argmax(self.logits), self.logits.shape[1])
-    self.selections = K.in_train_phase(samples, discrete_logits, training)
-    Y = K.dot(X, K.transpose(self.selections))
-    return Y
-
-  def compute_output_shape(self, input_shape):
-    return (input_shape[0], self.output_dim)
-
-class StopperCallback(EarlyStopping):
-  def __init__(self, mean_max_target = 0.998):
-    self.mean_max_target = mean_max_target
-    super(StopperCallback, self).__init__(monitor = '', patience = float('inf'), verbose = 1, mode = 'max', baseline = self.mean_max_target)
-    
-  def on_epoch_begin(self, epoch, logs = None):
-    print('mean max of probabilities:', self.get_monitor_value(logs), '- temperature', K.get_value(self.model.get_layer('concrete_select').temp))
-    #print( K.get_value(K.max(K.softmax(self.model.get_layer('concrete_select').logits), axis = -1)))
-    #print(K.get_value(K.max(self.model.get_layer('concrete_select').selections, axis = -1)))
-
-  def get_monitor_value(self, logs):
-    monitor_value = K.get_value(K.mean(K.max(K.softmax(self.model.get_layer('concrete_select').logits), axis = -1)))
-    return monitor_value
-
 
 class tinyLayerE(Layer):
   def __init__(self, output_dim, u, bins, start_temp=10.0, min_temp=0.1, alpha=0.99999, **kwargs):
@@ -188,28 +144,6 @@ class StopperCallback(EarlyStopping):
     monitor_value = K.get_value(K.mean(K.max(K.softmax(self.model.get_layer('tinyLayerE').logits), axis = -1)))
     return monitor_value
 
-class tinyLE(Layer):
-  def __init__(self, output_dim, u, bins, start_temp=10.0, min_temp=0.1, alpha=0.999$
-    self.output_dim=output_dim
-    self.u=K.constant(u)
-    self.start_temp = start_temp
-    self.min_temp = K.constant(min_temp)
-    self.alpha = K.constant(alpha)
-    super(tinyLE, self).__init__(**kwargs)
-
-  def build(self,input_shape):
-    self.tinyW=self.add_weight(name='tinyW', shape=(bins,self.output_dim), initializ$
-    super(tinyLE,self).build(input_shape)
-
-  def call(self, X, training = None):
-    al=K.softmax(K.dot(self.u,self.tinyW))
-    #al=K.transpose(al) 
-    Y = K.dot(X, al)
-    return Y
-
-  def compute_output_shape(self, input_shape):
-    return (input_shape[0], self.output_dim)
-
 
 # the data, split between train and test sets
 data=spio.loadmat(datafile)
@@ -247,37 +181,6 @@ for ii in range(0,num_exp):
   steps_per_epoch = (len(x_train) + batch_size - 1) // batch_size
   alpha = math.exp(math.log(min_temp / start_temp) / (num_epochs * steps_per_epoch))
 
-  ############################
-  ##  CAE
-  ############################
-
-  cinp1=Input(shape=(x_train.shape[1],))
-  cx=ConcreteSelect(nfeat,start_temp, min_temp, alpha, name = 'concrete_select')(cinp1)
-  cx = Dense(h_size*4)(cx)
-  cx = LeakyReLU(0.2)(cx)
-  cx = Dropout(0.2)(cx)
-  cx = Dense(h_size*2)(cx)
-  cx = LeakyReLU(0.2)(cx)
-  cx = Dropout(0.2)(cx)
-  cx = Dense(h_size)(cx)
-  cx = LeakyReLU(0.2)(cx)
-  cx = Dropout(0.2)(cx)
-  cx1 = Dense(h_size*2)(cx)
-  cx1 = LeakyReLU(0.2)(cx1)
-  cx1 = Dropout(0.2)(cx1)
-  cx1 = Dense(h_size*4)(cx1)
-  cx1 = LeakyReLU(0.2)(cx1)
-  cx1 = Dropout(0.2)(cx1)
-  cx1 = Dense(x_train.shape[1],name = 'recon')(cx1) #for tiny weights
-  cx2 = Dense(y_train.shape[1])(cx)
-  cx2 = Activation("softmax", name="classacc")(cx2)
-  cmodel = Model(inputs=cinp1, outputs=[cx1, cx2])
-  cmodel.compile(optimizer=opt, loss=losses, loss_weights=lossWeights, metrics=["accuracy","mse"])
-  chistory = cmodel.fit(x_train, {"recon": x_train, "classacc": y_train}, validation_data=(x_test, {"recon": x_test, "classacc": y_test}), epochs=num_epochs, verbose=1)
-  cindices = K.get_value(K.argmax(cmodel.get_layer('concrete_select').logits))
-  #cacc=np.add(cacc,chistory.history['val_classacc_accuracy'])
-  #closs=np.add(closs,chistory.history['val_recon_loss'])
-  #cmi=cmi+MI(X[:,cindices])
 
   ################################
   # FsNet
@@ -319,56 +222,9 @@ for ii in range(0,num_exp):
 
   indices = K.get_value(K.argmax(dl))
 
-  ################################
-  # DIET Network
-  ################################
-
-  dinp1=Input(shape=(x_train.shape[1],))
-  dx=tinyLE(nfeat,u_train,bins,start_temp, min_temp, alpha, name = 'tinyLE')(dinp1)
-  dx = Dense(h_size*4)(dx)
-  dx = LeakyReLU(0.2)(dx)
-  dx = Dropout(0.2)(dx)
-  dx = Dense(h_size*2)(dx)
-  dx = LeakyReLU(0.2)(dx)
-  dx = Dropout(0.2)(dx)
-  dx = Dense(h_size)(dx)
-  dx = LeakyReLU(0.2)(dx)
-  dx = Dropout(0.2)(dx)
-  dx1 = Dense(h_size*2)(dx)
-  dx1 = LeakyReLU(0.2)(dx1)
-  dx1 = Dropout(0.2)(dx1)
-  dx1 = Dense(h_size*4)(dx1)
-  dx1 = LeakyReLU(0.2)(dx1)
-  dx1 = Dropout(0.2)(dx1)
-  dx1 = tinyLayerD(x_train.shape[1],u_train,bins,name = 'recon')(dx1)
-  dx2 = Dense(y_train.shape[1])(dx)
-  dx2 = Activation("softmax", name="classacc")(dx2)
-  dmodel = Model(inputs=dinp1, outputs=[dx1, dx2])
-  dmodel.compile(optimizer=opt, loss=losses, loss_weights=lossWeights, metrics=["acc$
-  dhistory = dmodel.fit(x_train, {"recon": x_train, "classacc": y_train}, validation$
-  dacc=np.add(dacc,dhistory.history['val_classacc_accuracy'])
-  dloss=np.add(dloss,dhistory.history['val_recon_loss'])
-
-
-  #acc=np.add(acc,history.history['val_classacc_accuracy'])
-  #loss=np.add(loss,history.history['val_recon_loss'])
-  #mi=mi+MI(X[:,indices])
-  #print('Completed Experiments: '+str(ii))
-  #print(cindices)
-  #print(indices)
-  #print(K.get_value(model.get_layer('tinyLayerE').sf))
-  chist_df = pd.DataFrame(chistory.history)
-  chist_csv_file = rd+ds+"_"+str(nfeat)+"_"+str(ii)+"_chistory.csv"
-  with open(chist_csv_file, mode='w') as f:
-    chist_df.to_csv(f)
   hist_df = pd.DataFrame(history.history)
   hist_csv_file = rd+ds+"_"+str(nfeat)+"_"+str(ii)+"_history.csv"
   with open(hist_csv_file, mode='w') as f:
     hist_df.to_csv(f)
-  dhist_df = pd.DataFrame(dhistory.history)
-  dhist_csv_file = rd+ds+"_"+str(nfeat)+"_"+str(ii)+"_dhistory.csv"
-  with open(dhist_csv_file, mode='w') as f:
-    dhist_df.to_csv(f)
-  spio.savemat(rd+ds+"_"+str(nfeat)+"_"+str(ii)+'_cindices.mat', {'cindices': cindices})
   spio.savemat(rd+ds+"_"+str(nfeat)+"_"+str(ii)+'_indices.mat', {'indices': indices})
 
