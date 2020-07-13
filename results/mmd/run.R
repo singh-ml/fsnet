3d86fcbeb9a67f6f16b3d5194ef6a1f67a78976e
hclimente <hclimente@fei.local>
2020-07-13 12:09:38 +0200

    added resample procedure

--- results/mmd/run.R
+++ results/mmd/run.R
@@ -6,16 +6,25 @@ args = commandArgs(trailingOnly=TRUE)
 tissue = args[1]
 phenotype_file = args[2]
 
+# create gene expression matrix
 xpr <- read_tsv(paste0('esets_', tissue, '_exprs.txt'))
 X <- as.matrix(select(xpr, -X1))
 genes <- xpr$X1
 
+# create phenotype vector
 pheno <- read_tsv(phenotype_file)
 y <- as.numeric(pheno$`Characteristics[disease]` == 'tumor')
 names(y) <- pheno$`Source Name`
 
 y <- y[colnames(X)]
 
+# sample minor class to balance dataset
+cnt <- table(y)
+resamples <- sample(names(y)[y == names(which.min(cnt))], max(cnt) - min(cnt), replace = TRUE)
+
+X <- cbind(X, X[,resamples])
+y <- c(y, y[resamples])
+
 writeMat(paste0(tissue, '.mat'), X = t(X), Y = y, genes = genes)
 
-system2('python', args = c('../../../exp.py', './', paste0(tissue, '.mat), 10, 16,))
+system2('python', args = c('../../../exp.py', './', paste0(tissue, '.mat'), 10, 16))
